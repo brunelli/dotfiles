f23b6ca8eb510613b7bdb13c5ccbe00cb437e462
Ian Brunelli <ian@brunelli.me>
2019-05-19 08:33:41 -0300

    Fish: Fix Termux upgrade commands

--- termux/.config/fish/functions/up.fish
+++ termux/.config/fish/functions/up.fish
@@ -1,11 +1,10 @@
 function up
     echo ":: Upgrading system packages"
-    apt-get update &&
-    apt-get upgrade
+    pkg upgrade
     echo ":: Upgrading pip packages"
-    set upip (pip list --outdated --format json 2> /dev/null | awk -F'"' '{for(i=1;i<=NF;i++){if($i=="name"){NAME=i+2; printf $NAME" "}}}')
-    if [ $upip ]
-        pip install --upgrade $upip
+    set upip (pip list --outdated --format json 2> /dev/null | awk -F'"' '{for(i=1;i<=NF;i++){if($i=="name"){NAME=i+2; printf "%s " $NAME}}}')
+    if test -n "$upip"
+        printf "%s" $upip | xargs pip install --upgrade
     else
         echo "Nothing to upgrade."
     end
