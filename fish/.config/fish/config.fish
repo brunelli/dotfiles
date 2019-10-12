0d8ef1f75785bafae87db2efa80672d4eb0adb8b
Ian Brunelli <ian@brunelli.me>
2019-10-12 19:42:03 -0300

    Migrate from pass to KeepassXC

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,9 +3,14 @@ if status is-login
     set -gx EDITOR vim
     set -gx fish_user_paths $fish_user_paths ~/.local/bin 
     set -gx KITTY_ENABLE_WAYLAND 1
+    set -gx QT_QPA_PLATFORM wayland
     set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
-    set -gx SSH_AGENT_PID ''
-    set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+    if test -z (pgrep ssh-agent)
+        eval (ssh-agent -c) > /dev/null
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+        set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+        set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    end
     set -gx XKB_DEFAULT_LAYOUT br
     set -gx XKB_DEFAULT_MODEL abnt2
     set -gx XKB_DEFAULT_VARIANT abnt2 
