890f3b50480ef74de7035218af0b7428ad7926aa
Ian Brunelli <ian@brunelli.me>
2019-10-27 09:55:51 -0300

    Fish: Set $GOPATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,8 @@
 if status is-login
     set -gx CM_DIR ~/.local/share/
     set -gx EDITOR vim
-    set -gx fish_user_paths $fish_user_paths ~/.local/bin 
+    set -gx GOPATH ~/.local/share/go
+    set -gx fish_user_paths $fish_user_paths ~/.local/bin:$GOPATH/bin
     set -gx KITTY_ENABLE_WAYLAND 1
     set -gx QT_QPA_PLATFORM wayland
     set -gx QT_WAYLAND_DISABLE_WINDOWDECORATION 1
